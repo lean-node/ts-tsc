4fb595d6ffc8883e8b84459a6a7bfe4b2460e87b
Micha Alt <mail@lean-stack.de>
2021-06-14 20:34:30 +0200

    Add app tests. Just a plain ES test - no typescript used.

---
+++ __tests__/app.ts
@@ -0,0 +1,6 @@
+describe('Simple TypeScript Hello World Server', () => {
+  it('can handle tests written in TypeScript', () => {
+    const expected = true;
+    expect(true).toBe(expected);
+  });
+});
