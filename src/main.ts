683e2a2d9c0376f27bb0f3cff1809bf572528ac4
Micha Alt <mail@lean-stack.de>
2021-06-14 19:55:50 +0200

    Enforce code style.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import http from "http";
+import http from 'http';
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World!");
+  res.end('Hello World!');
 });
 
 server.listen(3000, () => {
-  console.log("Server started. Listening on port 3000 ...");
+  console.log('Server started. Listening on port 3000 ...');
 });
