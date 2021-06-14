9e14e250bd685f336e06c41ff311c0ece83b37eb
Micha Alt <mail@lean-stack.de>
2021-06-14 16:46:32 +0200

    Implement minimal server and provide scripts to build and start.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import * as http from "http";
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World!");
+});
+
+server.listen(3000, () => {
+  console.log("Server started. Listening on port 3000 ...");
+});
