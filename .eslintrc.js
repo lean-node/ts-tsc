6c9b6577e4d0d3a1e6d5ab69e899303889eaa960
Micha Alt <mail@lean-stack.de>
2021-06-14 17:54:50 +0200

    Adapt eslint configuration for project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  root: true,
+  env: {
+    node: true,
+    es2020: true,
+  },
+  rules: {},
+  ignorePatterns: [".eslintrc.js"],
 };
